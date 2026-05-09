5aadce8de0a324cb2fb424418423fb9ae3fa635a
Nick <nick@n2>
2026-05-08 21:20:40 -0400

    Bumped version

--- MenuLib/Entry.cs
+++ MenuLib/Entry.cs
@@ -11,7 +11,7 @@ using Object = UnityEngine.Object;
 
 namespace MenuLib;
 
-[BepInPlugin("nickklmao.menulib", MOD_NAME, "2.5.2")]
+[BepInPlugin("nickklmao.menulib", MOD_NAME, "2.5.3")]
 internal sealed class Entry : BaseUnityPlugin
 {
     private const string MOD_NAME = "Menu Lib";
